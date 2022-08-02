ed5b9997188074d9e9657c786d2e2d1c445b269b
Polyane Ribeiro <poly.pequena@gmail.com>
2022-08-02 20:11:23 -0300

    exercicio de estrutura de dados

---
+++ estrutura.js
@@ -0,0 +1,40 @@
+//lista, 3 objetos, 7 - dados, 3 strings, 2 bool, 1 number, 1 dado(objeto, 2 strings)
+
+const funcionario = [
+    {
+        nome: "fulaninho",
+        setor: "frente de caixa",
+        lider: "Jessica",
+        ativo: true,
+        idade: 19,
+        bateuMeta: true,
+        analiseFunc: {
+            qualidade: "Proativo",
+            pontoMelhora: "desatenção"
+        }
+    },
+    {
+        nome: "Beltraninho",
+        setor: "loja",
+        lider: "Luan",
+        ativo: true,
+        idade: 17,
+        bateuMeta: false,
+        analiseFunc: {
+            qualidade: "Agil",
+            pontoMelhora: "rude com clientes"
+        }
+    },
+    {
+        nome: "Mariazinha",
+        setor: "Almoxarifado",
+        lider: "Fernanda",
+        ativo: true,
+        idade: 23,
+        bateuMeta: true,
+        analiseFunc: {
+            qualidade: "atenciosa",
+            pontoMelhora: "ansiosa"
+        }
+    },
+]
