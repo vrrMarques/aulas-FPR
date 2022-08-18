2b8e70e3f0a9aea3159b63d75dce5402ab097533
Polyane Ribeiro <poly.pequena@gmail.com>
2022-08-18 17:12:46 -0300

    aulaget

--- src/App.js
+++ src/App.js
@@ -1,17 +1,79 @@
 
 import './App.css';
-import {teste} from './utils'
-import {Click} from './utils'
+import { useEffect, useState } from 'react';
+import axios from 'axios'
+
 function App() {
-  
+  const [resposta, setResposta] = useState();
+
+  //fetch('https://teste-aula-metodos-ad558-default-rtdb.firebaseio.com/jogos.json')
+  //  .then(res => res.json())
+  //  .then(data => setResposta(data))
+
+  useEffect(() => {
+    axios.get('https://teste-aula-metodos-ad558-default-rtdb.firebaseio.com/jogos.json')
+      .then(function (response) {
+        //console.log(response)
+        setResposta(response.data);
+      })
+
+  }, [])
+
+
 
   return (
-    <div className="App">
-      <form>
-        <input onChange={(e)=>{teste(e.target.value)}}/>
-        <button onClick={()=>{Click()}}>Mensagem</button>
-      </form>
-    </div>
+    <>
+      header<br />
+      {resposta &&
+        Object.values(resposta?.apex).map(jogo => {
+          console.log(resposta)
+          return (
+            <div>
+              <h2>apex</h2>
+              <p>{jogo.tipo}</p>
+              <p>{jogo.idade}</p>
+              <p>{jogo.empresa}</p>
+            </div>
+          )
+        })
+      }
+      {resposta &&
+        Object.values(resposta?.lol).map(jogo => {
+          return (
+            <div>
+              <h2>lol</h2>
+              <p>{jogo.tipo}</p>
+              <p>{jogo.idade}</p>
+              <p>{jogo.empresa}</p>
+            </div>
+          )
+        })
+      }
+      {resposta &&
+        Object.values(resposta?.wow).map(jogo => {
+          return (
+            <div>
+              <h2>wow</h2>
+              <p>{jogo.tipo}</p>
+              <p>{jogo.idade}</p>
+              <p>{jogo.empresa}</p>
+            </div>
+          )
+        })
+      }
+      {resposta &&
+        Object.values(resposta?.valorant).map(jogo => {
+          return (
+            <div>
+              <h2>valorant</h2>
+              <p>{jogo.tipo}</p>
+              <p>{jogo.idade}</p>
+              <p>{jogo.empresa}</p>
+            </div>
+          )
+        })
+      }
+    </>
   );
 }
 
